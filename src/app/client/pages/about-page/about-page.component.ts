395caf6824eaf3be9c39b3c61ff1d38b7fa07b40
salihou001 <abous0396@gmail.com>
2025-02-16 00:17:56 +0100

    merge work on branch new service

--- src/app/client/pages/about-page/about-page.component.ts
+++ src/app/client/pages/about-page/about-page.component.ts
@@ -1,7 +1,13 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { MissionSectionComponent } from '../../../shared/components/mission-section/mission-section.component';
 import { TeamCarouselComponent } from '../../../shared/components/team-carousel/team-carousel.component';
 import { ClientsCarouselComponent } from '../../../shared/components/clients-carousel/clients-carousel.component';
+import SplitType from 'split-type';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+gsap.registerPlugin(ScrollTrigger);
+
+
 @Component({
   selector: 'app-about-page',
   standalone: true,
@@ -9,6 +15,86 @@ import { ClientsCarouselComponent } from '../../../shared/components/clients-car
   templateUrl: './about-page.component.html',  
   styleUrl: './about-page.component.scss'
 })
-export class AboutPageComponent {
+export class AboutPageComponent implements AfterViewInit {
 
+  @ViewChild('titleAbout') titleAbout!: ElementRef;
+  @ViewChild('description') description!: ElementRef;
+  @ViewChild('textAnimation') textAnimation!: ElementRef;
+  @ViewChild('img1') img1!: ElementRef;
+  @ViewChild('img2') img2!: ElementRef;
+  @ViewChild('img3') img3!: ElementRef;
+
+  ngAfterViewInit(): void {
+    const title = new SplitType(this.titleAbout.nativeElement, { types: 'chars' });
+    const paragraph = new SplitType(this.description.nativeElement, { types: 'words' });
+
+    const TL = gsap.timeline();
+    TL
+    .fromTo(title.chars, {
+      y: 30,
+      opacity: 0
+    }, { 
+      y: '*',
+      delay: 1.3,
+      stagger: { amount: .1 },
+      opacity: 1,
+      ease: 'power1.inOut'
+    })
+    .fromTo(paragraph.words, {
+      y: 30,
+      opacity: 0
+    }, { 
+      y: '*',
+      delay: 1,
+      stagger: { amount: .3 },
+      opacity: 1,
+      ease: 'power1.inOut'
+    }, "<")
+    .fromTo(this.img2.nativeElement, {
+      y: -50,
+      opacity: 0
+    }, { 
+      y: '*',
+      delay: .1,
+      opacity: 1,
+      ease: 'power1.inOut'
+    })
+    .fromTo(this.img1.nativeElement, {
+      x: 50,
+      opacity: 0
+    }, { 
+      x: '*',
+      opacity: 1,
+      ease: 'power1.inOut'
+    }, "<.5")
+    .fromTo(this.img3.nativeElement, {
+      x: -50,
+      opacity: 0
+    }, { 
+      x: '*',
+      opacity: 1,
+      ease: 'power1.inOut'
+    }, "<")
+
+    const splitText = new SplitType(this.textAnimation.nativeElement, { types: 'chars' });
+
+    setTimeout(() => {
+
+      gsap.fromTo(
+        splitText.chars,
+        { color: '#999' },
+        {
+          color: '#000', 
+          stagger: 0.05,
+          scrollTrigger: {
+            trigger: this.textAnimation.nativeElement,
+            start: 'top 80%', 
+            end: 'top 20%',
+            scrub: true,
+          }
+        }
+      );
+    }, 300)
+  }
+  
 }
--- src/app/client/pages/home/home.component.ts
+++ src/app/client/pages/home/home.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TemoignageComponent } from '../../../shared/components/temoignage/temoignage.component';
 import { ContactFormComponent } from '../../../shared/components/contact-form/contact-form.component';
 import { ArticlesComponent } from '../../../shared/components/articles/articles.component';
 import { RouterModule, Router} from '@angular/router';
 import { ServiceDataService } from '../../../shared/sevices/service-data.service';
+import SplitType from 'split-type';
 
 @Component({
   selector: 'app-home',
@@ -19,7 +20,135 @@ export class HomeComponent implements OnInit {
     private router: Router 
   ) { }
   isSmallScreen: boolean = false;
+  @ViewChild('textPara') paragraph!: ElementRef;
+  @ViewChild('title') title!: ElementRef;
+  @ViewChild('textAnimation') textAnimation!: ElementRef;
 
+  ngAfterViewInit() {
+    const title = new SplitType(this.title.nativeElement, { types: 'chars' });
+    const paragraph = new SplitType(this.paragraph.nativeElement, { types: 'words' });
+    
+    const TL = gsap.timeline();
+    TL
+    .from('.image-container', {
+      delay: 1.3,
+      x: '50', 
+      opacity: 0
+    })
+    .from(title.chars, {
+      y: 30,
+      delay: .3,
+      stagger: { amount: .4 },
+      opacity: 0
+    }, "<.3")
+    .to(title.chars, {
+      y: '*',
+      opacity: 1,
+      ease: 'power1.inOut'
+    })
+    .from(paragraph.words, {
+      y: 30,
+      stagger: { amount: .4 },
+      delay: .5,
+      opacity: 0
+    }, "<-.5")
+    .to(paragraph.words, {
+      y: '*',
+      opacity: 1,
+      ease: 'power1.inOut'
+    })
+    .from('.search-container', {
+      x: '-80',
+      opacity: 0, 
+      ease: 'power2.inOut'
+    }, "<-.5")
+    .from('.team', {
+      y: '30',
+      opacity: 0, 
+      ease: 'power2.inOut'
+    }, "<")
+    .from('.clients', {
+      y: '30',
+      delay: .2,
+      opacity: 0, 
+      ease: 'power2.out'
+    }, "<.1")
+
+
+
+    const splitText = new SplitType(this.textAnimation.nativeElement, { types: 'chars' });
+
+    setTimeout(() => {  // ⚠️ Petit délai pour être sûr que les éléments sont rendus
+      if (this.services.length > 0) {
+        gsap.from(this.services.map(el => el.nativeElement), {
+          opacity: 0,
+          y: 100,
+          stagger: { amount: .5 },
+          ease: "power2.out",
+          scrollTrigger: {
+            trigger: ".services-grid",
+            start: "top 80%",
+            end: "top 30%",
+            scrub: true
+          }
+        });
+
+        gsap.from('.contact-content', {
+          opacity: 0,
+          x: -80,
+          ease: "power2.out",
+          scrollTrigger: {
+            trigger: ".contact-content",
+            start: "top 60%",
+            end: "top 30%",
+            scrub: true
+          }
+        });
+        gsap.from('.form', {
+          opacity: 0,
+          delay: .3,
+          x: 80,
+          ease: "power2.out",
+          scrollTrigger: {
+            trigger: ".form",
+            start: "top 60%",
+            end: "top 30%",
+            scrub: true
+          }
+        });
+
+        gsap.from(this.gridItems.map(el => el.nativeElement), {
+          opacity: 0,
+          y: 30,
+          stagger: { amount: .5 },
+          ease: "power2.out",
+          scrollTrigger: {
+            trigger: ".images-grid",
+            start: "top 80%",
+            end: "top 30%",
+            scrub: true
+          }
+        });
+
+        gsap.fromTo(
+          splitText.chars,
+          { color: '#999' },
+          {
+            color: '#000', 
+            stagger: 0.05,
+            scrollTrigger: {
+              trigger: this.textAnimation.nativeElement,
+              start: 'top 80%', 
+              end: 'top 20%',
+              scrub: true,
+            }
+          }
+        );
+      } else {
+        console.warn("⚠️ Aucun élément récupéré !");
+      }
+    }, 300); 
+  }
   ngOnInit(): void {
     this.checkScreenSize();
     window.addEventListener('resize', () => this.checkScreenSize());
