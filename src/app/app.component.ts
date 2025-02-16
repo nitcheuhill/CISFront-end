f17dc6005674f82545d8c63c66850ef2b721da41
salihou001 <abous0396@gmail.com>
2025-02-16 12:01:32 +0100

    adding animation on home page and about page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,34 +4,39 @@ import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { LoaderService } from './shared/sevices/loader.service';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './shared/components/footer/footer.component';
+import Lenis from '@studio-freight/lenis';
+
 import { LoaderComponent } from './shared/components/loader/loader.component';
-import { AuthService } from './shared/sevices/auth.service';
-import { NavbarbackofficeComponent } from './shared/components/navbarbackoffice/navbarbackoffice.component';
-import { ToastComponent } from './shared/components/toast/toast.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterModule,NavbarComponent,CommonModule,LoaderComponent,FooterComponent, NavbarbackofficeComponent,ToastComponent],
+  imports: [RouterModule,NavbarComponent,CommonModule,LoaderComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit{
   isLoading$;
-  isBackOffice = false;
 
-  constructor(private router: Router, private loaderService: LoaderService,private authService: AuthService) {
+  constructor(private router: Router, private loaderService: LoaderService) {
     this.isLoading$ = this.loaderService.isLoading;
-    this.router.events.subscribe(() => {
-      this.isBackOffice = this.router.url.startsWith('/back-office'); // Vérifie si on est sur le Backoffice
-    });
   }
 
   ngOnInit() 
   {
-    if (!this.authService.isLoggedIn() && this.router.url.includes('/dashboard')) {
-      this.router.navigate(['/login']);
-    }
 
+    const lenis = new Lenis();
+    lenis.on('scroll', ($e: any) => {
+      // console.log($e);
+    });
+    function raf(time: any) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+
+    if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+      //this.addAnimation();
+    }
     // Afficher le loader pendant le chargement initial
     this.loaderService.showLoader();
 
@@ -41,7 +46,7 @@ export class AppComponent implements OnInit{
         this.loaderService.showLoader();
       } else if (event instanceof NavigationEnd) {
         // Délai pour garantir le chargement complet de la page
-        setTimeout(() => this.loaderService.hideLoader(), 1000);
+        setTimeout(() => this.loaderService.hideLoader(), 500);
         //scrolle sur le haut de page
         window.scrollTo(0, 0); 
 
